efae5d8d2273b074fdabc61059f93dcef17db91b
PeterJanasz <petertjanasz@gmail.com>
2024-02-03 18:20:32 -0700

    navLinks and image background.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Sidenav from './components/sidenav'
+import Main from './components/Main'
+
+function App() {
+
+  return (
+    <>
+      <Sidenav />
+      <Main />
+    </>
+  )
+}
+
+export default App
