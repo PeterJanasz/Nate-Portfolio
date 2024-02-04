e15b3b4ee5a64ba8c7d8e64f5cca01d20f20b31f
PeterJanasz <petertjanasz@gmail.com>
2024-02-04 08:17:28 -0700

    fixed folder and file structure

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Main = () => {
   return (
     <div id='main'>
-        <img className='w-full h-screen object-cover ' src="public/images/rain_weather01_BW.JPG" alt="rainy day in a city" />
+        <img className='w-full h-screen object-cover ' src="/images/rain_weather01_BW.JPG" alt="rainy day in a city" />
         <div className='w-full h-screen absolute top-0 left-0'>
             <h1>I'm Nate</h1>
             <h2></h2>
